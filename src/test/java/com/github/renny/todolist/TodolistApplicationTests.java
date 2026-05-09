d12068280361f3eb6b673e66adfa08a531d02286
renny <meow3199@gmail.com>
2026-05-09 15:58:28 +0800

    chore:initialize Spring Boot project. add Spring Web,JPA and MySQL dependencies.

---
+++ src/test/java/com/github/renny/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.renny.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
