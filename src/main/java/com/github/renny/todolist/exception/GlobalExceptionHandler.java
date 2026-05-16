db2a6ac54c8caed62ed49de333ec0fd20bc4a099
renny <meow3199@gmail.com>
2026-05-16 22:37:28 +0800

    chore: add custom exception and global exception handler

---
+++ src/main/java/com/github/renny/todolist/exception/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.github.renny.todolist.exception;
+
+import com.github.renny.todolist.dto.response.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> resourceNotFound(ResourceNotFoundException e){
+        log.warn("搜尋失敗, {}", e.getMessage());
+        ApiResponse<Void> response = ApiResponse.error("搜尋失敗," + e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> formatValidationError(MethodArgumentNotValidException e){
+        String errorMessage = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        log.warn("資源不存在, {}" , errorMessage);
+        ApiResponse<Void> response = ApiResponse.error("格式錯誤," + errorMessage);
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleAll(Exception e){
+        log.error("發生非預期例外:" , e);
+        ApiResponse<Void> response = ApiResponse.error("伺服器發生錯誤,稍後再試");
+        return ResponseEntity.internalServerError().body(response);
+    }
+}
